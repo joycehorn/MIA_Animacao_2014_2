9ef78f581e07fe2c2085cdaed3d31bd633870283
Joyce Horn <hornjoyce@gmail.com>
2014-08-30 22:42:52 -0300

    Commit de projeto e merge

--- src/trabalho01/LancamentoObliquo.java
+++ src/trabalho01/LancamentoObliquo.java
@@ -1,9 +1,59 @@
 package trabalho01;
 
-public class LancamentoObliquo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.awt.GLCanvas;
+
+public class LancamentoObliquo extends GLCanvas implements KeyListener, GLEventListener{
 
 	public static void main(String[] args) {
 	
+	}
+
+	@Override
+	public void init(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void display(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width,
+			int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
